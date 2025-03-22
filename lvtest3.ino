ad687a6ccd9bf978bda805467955b1c2b8b40124
EJ <34107946+EJyoungc@users.noreply.github.com>
2025-03-22 07:37:17 +0200

    new

---
+++ lvtest3.ino
@@ -0,0 +1,20 @@
+#include "Display_ST7789.h"
+#include "LVGL_Driver.h"
+#include "ui.h"
+
+
+void setup() {
+  
+  // put your setup code here, to run once:
+  // LCD_Init();
+  // Set_Backlight(255);
+  
+ 
+  delay(1000);  
+
+}
+
+void loop(){
+    Timer_Loop();
+    delay(5);
+}
